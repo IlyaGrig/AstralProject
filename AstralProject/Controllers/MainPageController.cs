618b3f2ccb2f380b55d2d70c51f18f764da30083
Илья Григорьев <abrailia3@gmail.com>
2018-06-28 13:06:59 +0300

    AddNote work

--- AstralProject/Controllers/MainPageController.cs
+++ AstralProject/Controllers/MainPageController.cs
@@ -15,79 +15,13 @@ namespace AstralProject.Controllers
             return View();
         }
 
-        // GET: MainPage/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
 
-        // GET: MainPage/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		[HttpPost]
+		public ActionResult AddNewNote(string searchText)
+		{
 
-        // POST: MainPage/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+			return null;
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: MainPage/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: MainPage/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: MainPage/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: MainPage/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+		}
+	}
 }
