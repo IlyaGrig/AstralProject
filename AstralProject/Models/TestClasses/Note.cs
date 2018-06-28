197e2fe1c881e6cffe62c672af7401fea68a614d
Илья Григорьев <abrailia3@gmail.com>
2018-06-28 09:49:16 +0300

    Добавление AddNote (NWork)

---
+++ AstralProject/Models/TestClasses/Note.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AstralProject.Models.TestClasses
+{
+    public class Note
+    {
+		public static List<Note> GlobalNotes { get; } 
+		public string NoteName { get; set; }
+		public string HeaderNote { get; set; }
+		public string TextNote { get; set; }
+		public DateTime DateNote { get; set; }
+	    static Note()
+		{
+			GlobalNotes = new List<Note>();
+		}
+		public Note(string noteName,string headerNote,string textNote)
+		{
+			NoteName = noteName;
+			HeaderNote = headerNote;
+			TextNote = textNote;
+			DateNote = DateTime.Now;
+		}
+
+	}
+}
---
+++ AstralProject/Views/AddNote/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AstralProject.Models.TestClasses;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AstralProject.Views.AddNote
+{
+    public class IndexModel : PageModel
+    {
+		public string Mes = "";
+		public void OnGet()
+		{
+			Mes = "Создание заметки";
+		}
+		public void OnPost(string nameNote, string headerNote, string textNote)
+		{
+		
+			Note.GlobalNotes.Add(new Note(nameNote, headerNote, textNote));
+			Mes = $"Заметка {nameNote} добавлена";
+		}
+	}
+}
