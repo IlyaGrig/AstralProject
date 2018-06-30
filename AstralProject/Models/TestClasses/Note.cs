5c6d38be9383cfcf9046423f3d8173fbcb40a8ab
Илья Григорьев <abrailia3@gmail.com>
2018-06-30 23:38:50 +0300

    CRUD Notes work

--- AstralProject/Models/TestClasses/Note.cs
+++ AstralProject/Models/TestClasses/Note.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace AstralProject.Models.TestClasses
 {
     public class Note
     {
-		public static List<Note> GlobalNotes { get; } 
+		static int i = 0;
+		public static List<Note> GlobalNotes { get; set; }
+		public string Base64Icon { get; set; }
+		public int IdNote { get; set; }
 		public string NoteName { get; set; }
 		public string HeaderNote { get; set; }
 		public string TextNote { get; set; }
@@ -18,10 +24,28 @@ namespace AstralProject.Models.TestClasses
 		}
 		public Note(string noteName,string headerNote,string textNote)
 		{
+			i++;
+			IdNote = i;
 			NoteName = noteName;
 			HeaderNote = headerNote;
 			TextNote = textNote;
 			DateNote = DateTime.Now;
+			Base64Icon = AddIcon(IdNote);
+		}
+
+		string AddIcon(int idNote)
+		{
+			string imgInString64 = "";
+			using (var client = new HttpClient())
+			{
+				using (var response = client.GetStreamAsync($"https://avatars.dicebear.com/v2/identicon/{idNote}.svg").Result)
+				{
+					
+					
+				}
+			}
+			return imgInString64;
+
 		}
 
 	}
---
+++ AstralProject/Models/TestClasses/SettingsSearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AstralProject.Models.TestClasses
+{
+	public class SettingsSearch
+	{
+		public List<Note> InListNote { get; set; }
+		public string SearchText { get; set; }
+		public SettingsSearch(List<Note> inListNote, string searchText)
+		{
+			InListNote = inListNote;
+			SearchText = searchText;
+		}
+
+		public IEnumerable<Note> Search ()
+		{
+			return  InListNote.Where(e => e.HeaderNote.Contains(SearchText));
+													
+		}
+	}
+}
