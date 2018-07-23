0ec694fb274a04443cd50cdf3458936d84d574e4
Илья Григорьев <abrailia3@gmail.com>
2018-07-23 11:49:32 +0300

    NLayer

---
+++ BLL/TestClasses/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AstralProject.Models.TestClasses
+{
+    public interface IRepository 
+	{
+		List<Note> NoteCollection { get; set; }
+		List<ApplicationUser> UserCollection { get; set; }
+		IEnumerable<Note> UpdateCollection(Note note);
+		IEnumerable<Note> DeleteFromCollection(Note note);
+	}
+}
